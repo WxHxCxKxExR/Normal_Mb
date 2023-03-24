901a9bbf16e455a069ba36e4f13192ef7729cbde
normalhubv1 <128838097+WxHxCxKxExR@users.noreply.github.com>
2023-03-25 00:51:02 +0700

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/WxHxCxKxExR/Normal_Mb/main/BF-MB.lua"), true))();
+elseif game.PlaceId == 4520749081 or game.PlaceId == 6381829480 or game.PlaceId == 5931540094 or game.PlaceId == 6596144663 then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/WxHxCxKxExR/Normal_Mb/main/KL-MB.lua"), true))();
+else
+    game.Players.LocalPlayer:Kick("Normal Hub :: Not Support This Game")
+end
